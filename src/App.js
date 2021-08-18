de1a0e34546b266991678f07e3d49dcacfbc67a3
vinayak iyer <vinupapa8@gmail.com>
2021-08-18 20:45:55 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
+import React,{useEffect, useState} from 'react';
+//import DataTable from 'react-data-table-component';
+
+
+const API_HOST = "https://s3-ap-southeast-1.amazonaws.com/he-public-data/TopSellingGamesbb1c49e.json";
+const API_URL = `${API_HOST}/game-rank-api`;
+
 
 function App() {
+const [data,setData] = useState([]);
+
+//GET response from API
+const fetchData = () => {
+  fetch(`${API_HOST}`)
+  .then(res => res.json())
+  .then(json => setData(json));
+}
+
+//Calling function on component mount
+useEffect(()=>{
+  fetchData();
+},[]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="container">
+        <h1>Games Leader Board</h1>
+        <table>
+          <thead>
+            <tr>
+              <th>Rank</th>
+              <th>Name</th>
+              <th>Platform</th>
+              <th>Year</th>
+              <th>Genre</th>
+              <th>Publisher</th>
+              <th>Global Sales</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              data.map((item) =>(
+                <tr key={item.id}>
+                  <td>{item.Rank}</td>
+                  <td>{item.Name}</td>
+                  <td>{item.Platform}</td>
+                  <td>{item.Year}</td>
+                  <td>{item.Genre}</td>
+                  <td>{item.Publisher}</td>
+                  <td>{item.Global_Sales}</td>
+                </tr>
+              ))
+            }
+            
+          </tbody>
+
+        </table>
+
+      </div>
+
     </div>
   );
 }
---
+++ src/components/basic.table.js
@@ -0,0 +1,2 @@
+import React from "react";
+import {useTable} from 'react-table';
